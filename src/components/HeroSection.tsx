28b1efd13b91e05e233f17a5c1f7d422fa52c932
mangat-ram <se.mangat.ram@gmail.com>
2024-04-21 10:55:43 +0530

    Added Heading .

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -2,11 +2,29 @@ import React from "react";
 import MaxWidthWrapper from "./MaxWidthWrapper";
 import Link from "next/link";
 import Image from "next/image";
-import { buttonVariants } from "./ui/button";
+import { Button, buttonVariants } from "./ui/button";
 
 const HeroSection = () => {
   return (
-    <div>HeroSection</div>
+    <>
+      <MaxWidthWrapper
+        className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center"
+      >
+          <Button
+              className={buttonVariants({
+              size:"lg",
+              className:"-mt-10 text-xl rounded-full"
+            })}
+          >
+              Ethanol is Free
+          </Button>
+          <h1
+          className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl"
+        >
+          Efficiently Manage Your Projects: Introducing <span className="text-blue-600">Ethanol</span>
+        </h1>
+      </MaxWidthWrapper>
+    </>
   )
 }
 
