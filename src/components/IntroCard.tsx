8bd0fcf241f85700279a2df822c193ec120c9e7e
mangat-ram <se.mangat.ram@gmail.com>
2024-04-22 21:24:57 +0530

    made the text bolder.

--- src/components/IntroCard.tsx
+++ src/components/IntroCard.tsx
@@ -12,7 +12,7 @@ const IntroCard = () => {
       </div>
       <div className="w-full h-[450px] bg-yellow-600/50 rounded-3xl flex flex-col items-center justify-center gap-6">
         <p className="text-zinc-900 max-w-4xl text-3xl font-bold md:text-4xl lg:text-5xl font-featureDeck">Your Ultimate <span className="text-blue-400">Ethanol</span></p>
-        <p className="text-zinc-700 max-w-3xl text-2xl md:text-3xl lg:text-4xl font-featureDeck text-center">let no communication gap can interrupt your and your  team's workflow together work to detail on every aspect.</p>
+        <p className="text-zinc-700 max-w-3xl text-2xl md:text-3xl lg:text-4xl font-featureDeck font-bold text-center">let no communication gap can interrupt your and your  team's workflow together work to detail on every aspect.</p>
       </div>
     </div>
   )
