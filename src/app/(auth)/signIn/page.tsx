26f28053852274b612f91dbf4b3fc15d9449c8d8
mangat-ram <se.mangat.ram@gmail.com>
2024-04-25 23:53:25 +0530

    Added SignIn Form

--- src/app/(auth)/signIn/page.tsx
+++ src/app/(auth)/signIn/page.tsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React from "react"
+import { useToast } from "@/components/ui/use-toast"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useRouter } from "next/navigation"
+import { Button } from "@/components/ui/button"
+import * as z from "zod"
+import { useState } from "react"
+import { useForm } from "react-hook-form"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Loader2 } from 'lucide-react';
 
 const SignIn = () => {
   return (
-    <div>SignIn</div>
+    
   )
 }
 
---
+++ src/schemas/signUpSchema.ts
@@ -0,0 +1,20 @@
+import { z } from "zod";
+
+export const usernameVal = z
+  .string()
+  .min(3,"username must be atleast three chracters.")
+  .max(20,"username can have 20 chracters atmost.")
+  .regex(/^[a-zA-Z0-9_]+$/, "Username must not contain special chracters")
+
+export const nameVal = z
+  .string()
+  .min(3,"username must be atleast three chracters.")
+  .max(20,"username can have 20 chracters atmost.")
+  .regex(/^[a-zA-Z0-9_]+$/, "Username must not contain special chracters")
+
+export const signUpSchema = z.object({
+  username: usernameVal,
+  name: nameVal,
+  email: z.string().email({message:"Invalid Email Address"}),
+  password: z.string().min(6,{message : "Password must contain six letters."})
+})
