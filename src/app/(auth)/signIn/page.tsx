2c3d7961e8b2932c1472b0b0b3d41488e196ec2d
mangat-ram <se.mangat.ram@gmail.com>
2024-04-26 00:04:15 +0530

    Some font Changes.

--- src/app/(auth)/signIn/page.tsx
+++ src/app/(auth)/signIn/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react"
 import { useToast } from "@/components/ui/use-toast"
 import { zodResolver } from "@hookform/resolvers/zod"
@@ -39,7 +41,7 @@ const SignIn = () => {
   }
 
   return (
-      <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="font-cabinet flex justify-center items-center min-h-screen bg-gray-100">
         <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
           <div className="text-center">
             <h1 className="text-4xl font-bold tracking-tight lg:text-5xl mb-6 font-featureDeck">
