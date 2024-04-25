30974f328e13a05bc293d8271603de6c6a1fbcbd
mangat-ram <se.mangat.ram@gmail.com>
2024-04-26 00:08:08 +0530

    Building Register Form.

--- src/app/(auth)/signUp/page.tsx
+++ src/app/(auth)/signUp/page.tsx
@@ -1,8 +1,82 @@
-import React from 'react'
+import React from "react";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Loader2 } from 'lucide-react';
+import { signUpSchema } from "@/schemas/signUpSchema";
 
 const SignUp = () => {
+
+  const submitRegister = () => {
+    console.log("");
+    
+  }
+
   return (
-    <div>SignUp</div>
+          <div className="font-cabinet flex justify-center items-center min-h-screen bg-gray-100">
+        <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+          <div className="text-center">
+            <h1 className="text-4xl font-bold tracking-tight lg:text-5xl mb-6 font-featureDeck">
+              Join Ethanol Now
+            </h1>
+            <p className="mb-4">
+              register yourself
+            </p>
+          </div>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(submitLogin)} className="space-y-6">
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Username</FormLabel>
+                    <FormControl>
+                      <Input 
+                        placeholder="your username here" 
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input 
+                        type="password"
+                        placeholder="password" 
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit" disabled={isSubmitting}>
+                {
+                    isSubmitting ? (<>
+                      <Loader2 
+                        className="mr-2 h-4 w-4 animate-spin" 
+                      /> Please wait
+                    </>) : ("SignIn")
+                }
+              </Button>
+            </form>
+          </Form>
+        </div>
+      </div>
   )
 }
 
