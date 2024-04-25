011240ff65a8d7d548354c0c9cea0644a68d26ce
mangat-ram <se.mangat.ram@gmail.com>
2024-04-26 00:01:27 +0530

    Added SignIn Page form.

--- src/app/(auth)/signIn/page.tsx
+++ src/app/(auth)/signIn/page.tsx
@@ -16,10 +16,87 @@ import {
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { Loader2 } from 'lucide-react';
+import { signInSchema } from "@/schemas/signInSchema"
 
 const SignIn = () => {
-  return (
+
+  const [isSubmitting, setIsSubmitting] = useState(false)
+  const router = useRouter();
+  const { toast } = useToast();
+
+  const form = useForm<z.infer<typeof signInSchema>>({
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+      username:"",
+      password: ""
+    }
+  })
+
+  const submitLogin = async (data: z.infer<typeof signInSchema>) => {
+    // const result = await 
+    console.log("success");
     
+  }
+
+  return (
+      <div className="flex justify-center items-center min-h-screen bg-gray-100">
+        <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+          <div className="text-center">
+            <h1 className="text-4xl font-bold tracking-tight lg:text-5xl mb-6 font-featureDeck">
+              Join Ethanol Now
+            </h1>
+            <p className="mb-4">
+              enter email and password to Sign In
+            </p>
+          </div>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(submitLogin)} className="space-y-6">
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Username</FormLabel>
+                    <FormControl>
+                      <Input 
+                        placeholder="your username here" 
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input 
+                        type="password"
+                        placeholder="password" 
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit" disabled={isSubmitting}>
+                {
+                    isSubmitting ? (<>
+                      <Loader2 
+                        className="mr-2 h-4 w-4 animate-spin" 
+                      /> Please wait
+                    </>) : ("SignIn")
+                }
+              </Button>
+            </form>
+          </Form>
+        </div>
+      </div>
   )
 }
 
