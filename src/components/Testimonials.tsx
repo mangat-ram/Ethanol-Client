20f4dd54ba8c8ffaeb523bb2db20580cae490d87
mangat-ram <se.mangat.ram@gmail.com>
2024-04-28 13:55:20 +0530

    Completed Testimonials section

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -19,19 +19,19 @@ interface TestimonialsProps{
 
 const Testimonials = ({avatar,name,role,companyName,review,className}:TestimonialsProps) => {
   return (
-      <Card className={cn("flex w-[30%]", className)}>
-        <CardHeader className="flex items-center text-center">
+      <Card className={cn("flex flex-col w-[30%]", className)}>
+        <CardHeader className="flex text-center flex-row justify-between gap-4">
           <div className="relative w-16 h-16">
-            <Image className="absolute rounded-full inset-0 w-full h-full object-cover" src={avatar} alt="cn" quality={100} width={64} height={64} />
+            <Image className="absolute rounded-lg inset-0 w-full h-full object-cover" src={avatar} alt="cn" quality={100} width={64} height={64} />
           </div>
-          <div>
+          <div className="mr-16">
             <CardTitle>{name}</CardTitle>
             <CardTitle className="text-sm font-medium">{role}</CardTitle>
             <CardTitle className="text-sm font-bold">{companyName}</CardTitle>
           </div>
         </CardHeader>
         <CardContent className="flex-grow">
-          <p className="text-[1.04rem] text-left pt-6">{review}</p>
+          <p className="text-[1.04rem] text-left">{review}</p>
         </CardContent>
       </Card>
   )
